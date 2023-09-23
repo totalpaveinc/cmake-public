daec27fa934ff956909931ecc71177c8eaec2212
Norman Breau <norman@nbsolutions.ca>
2023-09-23 19:25:45 -0300

    DownloadArchive

--- DownloadArchive.cmake
+++ DownloadArchive.cmake
@@ -31,49 +31,6 @@ function(DownloadArchive TARGET_NAME URL HASH)
 
     file(ARCHIVE_EXTRACT INPUT ${SOURCE_DIR}/${FILENAME} DESTINATION ${CMAKE_BINARY_DIR}/${TARGET_NAME})
 
-    set(${TARGET_NAME}_DownloadArchive download_${TARGET_NAME})
+    # set(${TARGET_NAME}_DownloadArchive download_${TARGET_NAME} PARENT_SCOPE)
     set(${TARGET_NAME}_content_path ${CMAKE_BINARY_DIR}/${TARGET_NAME} PARENT_SCOPE)
 endfunction()
-
-# function(DownloadDependency TARGET_NAME DEP_URL DEP_SHA1)
-#     string(FIND "${DEP_URL}" "/" DEP_FILENAME REVERSE)
-#     set(DEP_FILE "${CMAKE_BINARY_DIR}/${DEP_FILE_NAME}")
-#     set(EXTRACTION_DIR "${CMAKE_BINARY_DIR}/${TARGET_NAME}")
-
-#     if(NOT EXISTS "${ZIP_FILE}" OR NOT EXISTS "${EXTRACTION_DIR}")
-#         message(STATUS "Downloading and extracting ${TARGET_NAME}...")
-        
-
-#         FetchContent_Declare(download_${hash}
-# URL ${url}
-# URL_HASH SHA256=${hash}
-# DOWNLOAD_NO_EXTRACT true
-# TLS_VERIFY true
-# )
-
-#         # file(WRITE "${CMAKE_BINARY_DIR}/{TARGET_NAME}.sha1" "${DEP_SHA1}")
-#         # file(DOWNLOAD ${DEP_URL} ${DEP_FILE} EXPECTED_HASH SHA1=${DEP_SHA1})
-
-
-#     else()
-#         message(STATUS "${TARGET_NAME} is already downloaded and extracted.")
-#     endif()
-
-# endfunction()
-
-# function(download_and_extract_zip TARGET_NAME ZIP_URL ZIP_SHA1)
-#     set(ZIP_FILE "${CMAKE_BINARY_DIR}/${TARGET_NAME}.zip")
-#     set(EXTRACTION_DIR "${CMAKE_BINARY_DIR}/${TARGET_NAME}")
-
-#     if(NOT EXISTS "${ZIP_FILE}" OR NOT EXISTS "${EXTRACTION_DIR}")
-#         message(STATUS "Downloading and extracting ${TARGET_NAME}...")
-#         file(DOWNLOAD ${ZIP_URL} ${ZIP_FILE} EXPECTED_HASH SHA1=${ZIP_SHA1})
-
-#         execute_process(COMMAND ${CMAKE_COMMAND} -E tar xzf ${ZIP_FILE}
-#                         WORKING_DIRECTORY ${CMAKE_BINARY_DIR})
-
-#         message(STATUS "${TARGET_NAME} downloaded and extracted.")
-#     else()
-#         message(STATUS "${TARGET_NAME} is already downloaded and extracted.")
-#     endif()
-# endfunction()
